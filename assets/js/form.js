893fbc7f65c808f7b5ac6888c22a91353f8079df
KismiOfficial <kismisolutions@gmail.com>
2025-02-04 00:50:25 -0800

    Update Form JS

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,3 +1,37 @@
 (function() {
+    console.log('Test!');
+    
+    const button = document.getElementById('post-btn');
 
+    button.addEventListener('click', async _ => {
+        console.log('Inside!');
+        const settings = {
+        method: 'POST',
+        defaultJson: true,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer pat-na1-be681d2e-e7d8-4cb9-8f5c-47783a93c1ba',
+        },
+        body: {
+            "properties": {
+                "email": "testuseremail@gmail.com",
+                "firstname": "John",
+                "lastname": "Smith"
+            }
+        }
+        };
+        
+        try {
+            console.log('Here!');
+            const fetchResponse = await fetch(`https://api.hubapi.com/crm/v3/objects/contacts`, settings);
+            const data = await fetchResponse.json();
+            console.log(data);
+            console.log('End!');
+            return data;
+            } catch (e) {
+                console.log(e);
+                return e;
+            }    
+    });
+    
 })();
