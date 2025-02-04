64bb4981e7eb14f3014131e056c84552a88c3231
KismiOfficial <kismisolutions@gmail.com>
2025-02-04 01:11:39 -0800

    Update form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -10,8 +10,8 @@
         mode: 'no-cors',
         defaultJson: true,
         headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer pat-na1-be681d2e-e7d8-4cb9-8f5c-47783a93c1ba',
+            Content-Type: 'application/json',
+            Authorization: 'Bearer pat-na1-be681d2e-e7d8-4cb9-8f5c-47783a93c1ba',
         },
         body: {
             "properties": {
