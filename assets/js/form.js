5545843636ce97cdcb7451702a37660d19d5460e
KismiOfficial <kismisolutions@gmail.com>
2025-02-04 01:07:07 -0800

    Update form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -7,6 +7,7 @@
         console.log('Inside!');
         const settings = {
         method: 'POST',
+        mode: 'no-cors',
         defaultJson: true,
         headers: {
             'Content-Type': 'application/json',
